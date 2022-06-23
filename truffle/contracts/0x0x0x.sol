7b27bbb9cd90ae3abf33442c050fcf4d80d55c3d
danderic6ister <danderick6ister@gmail>
2022-06-24 00:06:00 +0100

    Wrote out other functions and changed some configurations

--- truffle/contracts/0x0x0x.sol
+++ truffle/contracts/0x0x0x.sol
@@ -6,96 +6,103 @@ import "../node_modules/@openzeppelin/contracts/utils/cryptography/MerkleProof.s
 import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
+
+
 contract x0x0x is ERC721,ERC721Enumerable,Ownable{
-    // Returns whether or not the contract is active 
-    bool    public isActive = false ;
+    /**  @dev Returns whether or not the contract is active */
+    bool    public isActive ;
 
-    // Tracks the state of the whitelistMint and PublicMint
-    bool public whitelistMintIsActive = false;
-    bool public publicMintIsActive = false;
+    /**  Tracks the state of the whitelistMint and PublicMint */
 
-    // @dev Sets the total Supply of the NFT collection 
+    /**  @dev Sets the total Supply of the NFT collection  */
     uint256 public maxSupply = 200;
-    // Dev Sets the price of whitelist sale
+
+    /**  @dev Sets the price of whitelist sale */
     uint256 public immutable whitelistPrice = 0.01 ether;
-    // Dev sets the price of Public Sale 
+
+    /**  @dev sets the price of Public Sale  */ 
     uint256 public  publicPrice = 0.02 ether ;
-    // DEv sets the base uri of th collection
+
+    /**  @dev sets the base uri of th collection */
     string public baseURI;
 
-    // Dev sets the TeamSupply
-    uint256 public teamSupply = 10;
+    /**  @dev sets the base uri of the collection  */
+    uint256 public teamSupply = 20;
 
-    // Dev sets the total amount available for airdrops 
+    /**  @dev sets the total amount available for airdrops  */
     uint256 public amountForAirdrops = 15;
 
-    // The MerkleRoot for Whitelisted Addresses
+    /**  @dev sets the MerkleRoot for Whitelisted Addresses */
     bytes32 public merkleRoot;
 
-    // Sets the max number of mint for Whitelist and PublicMint
+    /**  @dev  Sets the max number of mint for Whitelist and PublicMint */
     uint256 public maxMintForWhitelist = 2;
-    uint256 public maxMintForPublicSale    = 1;
+    /**
+         @dev Could have been useful if maxMint for public was more than 1
+      
+    // uint256 public maxMintForPublic  = 1;
+    */
 
-    // Sets the max number of transacions for Whitelist and Public 
-    uint256 public maxTxForPublicSaleMint = 1;
-    uint256 public maxTxForWhitelistMint = 1;
+    /**  @dev Sets the max number of transacions for Whitelist and Public  */ 
+   
+    uint256 public maxTx= 1;
+    
 
-    // Keeps tracks of number of nfts minted in Whitelist and PublicMint
-    mapping(address => uint256) public mintPerfomedInWhitelistMint;
-    mapping(address => uint256) public mintPerfomedInPublicSaleMint;
+    /**  @dev Keeps tracks of number of nfts minted in Whitelist and PublicMint  */
+    mapping(address => uint256) public mintPerformedInWhitelistMint;
+    mapping(address => uint256) public mintPerformedInPublicSaleMint;
 
-    // Keeps track of number of transactions perfomed in Whitelist and PublicMint
-    mapping(address => uint256) public txPerformedInWhitelisMint;
+    /**  @dev Keeps track of number of transactions perfomed in Whitelist and PublicMint  */
+    mapping(address => uint256) public txPerformedInWhitelistMint;
     mapping(address => uint256) public txPerformedInPublicSaleMint;
 
-    uint256 public timeContractIsActive;
+
+    /**  @dev Initializes the time the contract is deployed and hardcodes it for other variables  */
+    uint256 public timeContractIsActive = block.timestamp ;
     uint256 public whitelistMintStarts = timeContractIsActive + 20;
     uint256 public whitelistMintEnds = whitelistMintStarts + 400;
     uint256 public publicMintStarts = whitelistMintEnds +20;
+
     bool public whitelistIsActive = false;
-    bool public publicMintisActive = false;
+    bool public publicMintIsActive = false;
+    bool public teamHasClaimed ;
 
 
-    // Sets the name and symbol of the collection
-    constructor()ERC721("x0x0x","x0x0x"){
-        
-    }
-    // @dev checks if the contract is active 
+    /** @dev Sets the name and symbol of the collection */ 
+    constructor()ERC721("x0x0x","x0x0x") {}
+
+    /** @dev checks if the contract is active  */
     modifier contractIsActive(){
       require(isActive,"Contract is not yet active.");
      _;
     }
-    // sets the state of the contract to true 
-    function setContractActive(bool _isActive) public  onlyOwner returns(uint256){
-        require(isActive == false, "Contract is already active ");
-        require(_isActive,"Contract can only be set to true ");
-            
-        isActive = _isActive;
-        timeContractIsActive = block.timestamp;
-        return timeContractIsActive;
-
-    }
-    // @dev retuns Merkle root of whitelisted addresses
+    /** @dev retuns Merkle root of whitelisted addresses */
     function whitelistMintMerkleRoot() public view returns(bytes32){
         return merkleRoot;
     }
+    /**  @dev sets the merkleroot of whitelisted addresses  */
+    function setWhitelistRoot(bytes32 _whitelistRoot) public onlyOwner {
+        merkleRoot = _whitelistRoot;
+    }
+    /**  @dev verifies if wallet is whitelisted */  
+    function isWhiteListed(bytes32[] calldata proof) view internal returns(bool) {
+       bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
+        return MerkleProof.verify(proof, merkleRoot, leaf);
+    }
     
-    //   @dev Owner sets the base URI of the contract
+    /**  @dev Owner sets the base URI of the contract */ 
     function setBaseURI(string calldata BaseURI)public onlyOwner{
         baseURI = BaseURI;
     }
-    // @dev Function returns the baseURI of the contract
-    function _baseURI() internal view override returns (string memory) {
+    /**  @dev Function returns the baseURI of the contract */
+   function _baseURI() internal view override returns (string memory) {
         return baseURI;
     }
-    function supportsInterface(bytes4 interfaceId) public view virtual override( ERC721,ERC721Enumerable) returns (bool) {
-        return interfaceId == type(IERC721Enumerable).interfaceId || super.supportsInterface(interfaceId);
-    }
-    function setPriceForPublicMint(uint256 newPrice)private contractIsActive onlyOwner {
-        require(newPrice >publicPrice , "New Price must be greater than old price");
-        require(newPrice < 0.05, 'new price cannot be more than 0.05 eth');
-        publicPrice = newPrice;
-    }
+
+  /**   @dev function checks is the time for whitelistmint is up.
+        function is called before every mint to check the state of the contract.
+        
+ */
     function checksIfWhitelistMintIsActive() internal {
         if(block.timestamp > whitelistMintStarts && block.timestamp < whitelistMintEnds ){
             whitelistIsActive = true ;
@@ -103,6 +110,10 @@ contract x0x0x is ERC721,ERC721Enumerable,Ownable{
             whitelistIsActive = false;
         }
     }
+    /**   @dev function checks is the time for PublicMint is up.
+        function is called before every mint to check the state of the contract.
+        
+ */
     function checksIfPublicMintIsActive() internal{
         if (block.timestamp >= publicMintStarts){
             publicMintIsActive = true;
@@ -110,28 +121,115 @@ contract x0x0x is ERC721,ERC721Enumerable,Ownable{
             publicMintIsActive = false ;
         }
     }
-    function whiteListMint(uint256 mintAmount,bytes32[] proof) public contractIsActive {
+
+    /** @dev mints the team supply to thier wallet.
+        The team needs to mint to treasury after which the contract becomes active */
+
+    function mintToTeam() public onlyOwner {
+        require(!teamHasClaimed, "You have claimed already.");
+        for(uint i =0 ; i< teamSupply; i++){
+            _mint(msg.sender,totalSupply());
+        }
+        teamHasClaimed = true;
+        teamSupply =0;
+
+        isActive = true;
+
+    }
+    /**  @dev Allows owner ot change the price for publicSale recall the mint price for whitelist mint is immutable .
+              also converts inputted price to solidity's standard.
+              Ensures the upper value of price change does not exceed 0.05eth  */
+    function setPriceForPublicMint(uint256 newPrice)private contractIsActive onlyOwner {
+        uint256 newPriceInSolidity = newPrice * 100;
+        uint256 value = 5;
+        uint upperPriceLimit = value/100;
+
+       
+        require(newPriceInSolidity /100 > publicPrice , "New Price must be greater than old price");
+        require(newPriceInSolidity /100  < upperPriceLimit , 'new price cannot be more than 0.05 eth');
+        publicPrice = newPrice;
+    }
+    /**  @dev allows the public to mint after a certain time.
+            Checks if the contract is active , which can only be after the team has minted.
+            Checks if wl mint is active, reverts if true .
+            Also checks if public mint is active , reverts if false.
+            Requires the mint amount to be equal to 1.
+            Checks if wallet has sent a transaction exceeding the max,reverts if true.
+            checks if the minter is sending the right amount of ether to contract,reverts if false.
+            Checks if the the amount to be minted and the current supply does not cause an overflow.
+            mints the amount,uses the current totalSupply before mint as the id of the token to be minted.
+            increments the tx performed in the mint phase.
+            increments the mints perforemd in this phase
+
+     */
+    function publicMint(uint256 mintAmount) public payable contractIsActive {
+         checksIfWhitelistMintIsActive();
+         checksIfPublicMintIsActive();
+         require(!whitelistIsActive,"Whitelsit mint is still active.");
+         require(publicMintIsActive,"Public mint has not yet begun.");
+        //  @dev If you plan to mint more than 1 nfts in your public mint. Front end logic should cove rthus , but what if we mint from contract.
+        //  require(mintAmount > 0,"dumbass , you can't mint zero nfts.");
+
+        // @dev require mintAmount to be compulsorily equal to 1 because i dotn want more than 1 to be minted in this phase.
+         require(mintAmount == 1,"You cant mint more  or less than 1 nft");
+        //  require(mintPerfomedInPublicSaleMint[msg.sender]< 1,"You can only perform1 mint ")
+         require(txPerformedInPublicSaleMint[msg.sender] <maxTx,"You can onlt send one transaction from your address and you already did.");
+         require(publicPrice *mintAmount >= msg.value,"the amount you are abount to send is lees than what you should pay.");
+         require(totalSupply() + mintAmount <= maxSupply-amountForAirdrops-teamSupply, "Minted Out" );
+         _mint(msg.sender,totalSupply());
+
+         txPerformedInPublicSaleMint[msg.sender] ++;
+         mintPerformedInPublicSaleMint[msg.sender] ++;
+
+
+    } /**  @dev    allows the whitelisted addresses  to mint after a certain time.
+                Checks if the contract is active , which can only be after the team has minted.
+               checks if wallet is whitelisted.
+               Checks if wl mint is active, reverts if false .
+               Also checks if public mint is active , reverts if true.
+               Requires the mint amount to be equal to 1.
+               Checks if wallet has sent a transaction exceeding the max,reverts if true.
+               checks if the minter is sending the right amount of ether to contract,reverts if false.
+               Checks if the the amount to be minted and the current supply  causes an overflow.
+               -mints the amount,uses the current totalSupply before mint as the id of the token to be minted.
+               increments the tx performed in the mint phase.
+               increments the mints perforemd in this phase(could be useful for future airdrops)
+
+     */
+    function whiteListMint(uint256 mintAmount,bytes32[] calldata proof) public payable contractIsActive {
         checksIfWhitelistMintIsActive();
         checksIfPublicMintIsActive();
 
-        require(isWhiteListed(proof); "You are not whitelisted to mint this nft, you can wait for public mint.")
+        require(isWhiteListed(proof), "You are not whitelisted to mint this nft, you can wait for public mint.");
         require(!publicMintIsActive,"Public mint is already active");
         require(whitelistIsActive,"Whitelist mint has not yet begun");
         require(mintAmount > 0, "Why would you want to mint zero nfts, don't be dumb");
         require(mintAmount <= maxMintForWhitelist, "You can't mint more than 2 in this phase");
-        require(txPerformedForWhitelistMint[msg.sender] < maxTxForWhitelistMint, "You can only send one transaction from your address to this contract and you already did.");
+        require(mintPerformedInWhitelistMint[msg.sender] < maxMintForWhitelist, "You can only mint a max of 2 nfts  and you already did.");
+        require(txPerformedInWhitelistMint[msg.sender] < maxTx , "You can only send one transaction from your address to this contract and you already did.");
+        require(whitelistPrice * mintAmount >= msg.value , "The amount you are about to send is less than what you should pay.");
+        require(totalSupply() + mintAmount <= maxSupply - amountForAirdrops- teamSupply, "Minted out.");
+        uint _id = totalSupply();
+        for (uint256 i = 0; i<mintAmount; i++){
 
+            _mint(msg.sender,++_id);
 
+        }
+        txPerformedInWhitelistMint[msg.sender] ++;
         
-
-
-    }
-    function isWhiteListed(bytes32[] proof) internal returns(bool) {
-        leaf = keccak256(msg.sender);
-        return MerkleProof.verify(proof, MerkleRoot, leaf);
-
+        mintPerformedInWhitelistMint[msg.sender] += mintAmount;
 
     }
+  
+  
+  /** @dev withdraws funds from contract  */
+    function withdraw() public payable onlyOwner{
+        // address ContractOwner = msg.sender;
+
+        payable(owner()).transfer(address(this).balance);
+
+    }  
+  /** @dev routine  implementation for inheriting from  ERC721Enumerable.sol */  
 
     function _beforeTokenTransfer(
         address from,
@@ -143,22 +241,17 @@ contract x0x0x is ERC721,ERC721Enumerable,Ownable{
        
     }
 
-    function setBaseURI(string calldata BaseURI) public onlyOwner{
-        baseURI = BaseURI;
-    }
-     function supportsInterface(bytes4 interfaceId) public view  override(ERC721,ERC721Enumerable) returns (bool) {
-        return interfaceId == type(IERC721Enumerable).interfaceId || super.supportsInterface(interfaceId);
+   
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 tokenId
-     ) internal  override(ERC721,ERC721Enumerable) {
-        super._beforeTokenTransfer(from, to, tokenId);
-
-
-    }
+   
 
    
     
@@ -172,3 +265,14 @@ contract x0x0x is ERC721,ERC721Enumerable,Ownable{
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
